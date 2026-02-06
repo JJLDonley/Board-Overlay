80f5bcd07f6d55c1add009047858833ca3949fd5
Jeremiah donley <jaeldonley@gmail.com>
2026-02-05 20:20:42 -0500

    Remove legacy artifacts from repo

--- legacy/js/utils/dom-elements.js
+++
@@ -1,9 +0,0 @@
-// DOM element references
-export const FEED = document.getElementById("feed");
-export const CANVAS = document.getElementById("overlay");
-export const CONTEXT = CANVAS?.getContext("2d");
-export const VIDEO = document.getElementById("VideoButton");
-export const VIDEO_URL = document.getElementById("VideoURL");
-export const STYLE = document.getElementById("StyleButton");
-export const RESET = document.getElementById("ResetGrid");
-export const VDO_LINK = document.getElementById("vdo_link"); 
